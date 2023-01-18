4baa78ebf1e75dbd385f41b1c77c4f684adb3221
mungmung2j <warning6715@gmail.com>
2023-01-18 16:10:57 +0900

    ajax 2일차

--- client/main.js
+++ client/main.js
@@ -1,15 +1,25 @@
-import { xhrData,insertLast } from "./lib/index.js";
+
+import { 
+  xhrData,
+  insertLast,
+  xhrPromise,
+  tiger,
+  delayP
+} from "./lib/index.js";
 
 
 
 
 
-xhrData.get(
-  'https://jsonplaceholder.typicode.com/users/1',
-  (res)=>{
-    insertLast('body',JSON.stringify(res))
-  },
-  (err)=>{
-    insertLast('body','데이터 로딩에 실패했습니다.')
-  }
-)
+async function render(){
+
+  await delayP(2000);
+  let response = await tiger.get('https://jsonplaceholder.typicode.com/users/1')
+
+
+
+  console.log(response.data);
+}
+
+
+render()
